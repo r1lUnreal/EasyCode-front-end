7da21e1dfafbd7d194fc3c0aeb0f6d31f0d7c551
r1lUnreal <150723650+r1lUnreal@users.noreply.github.com>
2025-06-03 18:09:07 +0300

    home

--- script/script2.js
+++ script/script2.js
@@ -1,11 +1,85 @@
-let password = '1234'
-let count = 3
+// let ready_robot = 0;
 
-while (count > 0) {
-    let user_password = prompt('enter the password:')
-    count--
-    if (user_password == password) {
-        alert('good man!')
-        break
+// while (true) {
+//     let robot = prompt('Робот готов к работе? (да / нет. стоп - выйти из программы)');
+
+//     if (robot == 'стоп') {
+//         break;
+//     }
+//     else if (robot == 'да') {
+//         ready_robot++;
+//     }
+//     else if (robot == 'нет') {
+//         alert('Робот готов к работе? (да / нет. стоп - выйти из программы)');
+//     }
+// }
+
+// alert(`Колличество готовый роботов: ${ready_robot}`);
+
+
+// let ready_robot = 0; // готовы
+// let needsRepair_robot = 0; // требуется ремонт
+// let decommissioned_robot = 0; // списаны
+
+// while (true) {
+//     let robot = prompt('Какое состояние у робота?');
+
+//     if (robot == 'stop') {
+//         break;
+//     }
+//     else if (robot == 'ready') {
+//         ready_robot++;
+//     }
+//     else if (robot == 'needsRepair') {
+//         needsRepair_robot++;
+//     }
+//     else if (robot == 'decommissioned') {
+//         decommissioned_robot++;
+//     }
+// }
+
+// alert(`Состояние роботов: готовы к работе: ${ready_robot}, требуется ремонт: ${needsRepair_robot}, списаны с работы: ${decommissioned_robot}`);
+
+let ready_robot = 0; // готовы
+let needsRepair_robot = 0; // требуется ремонт
+let decommissioned_robot = 0; // списаны
+
+function inventoryRobots() {
+    while (true) {
+        let robot = prompt('Какое состояние у робота?');
+
+        if (robot == 'stop') {
+            break;
+        }
+        else if (robot == 'ready') {
+            confirm('Confirm this action?');
+            if (confirm == true) {
+                ready_robot++;
+            }
+            else {
+
+            }
+        }
+        else if (robot == 'needsRepair') {
+            confirm('Confirm this action?');
+            if (confirm == true) {
+                needsRepair_robot++;
+            }
+            else {
+
+            }
+        }
+        else if (robot == 'decommissioned') {
+            confirm('Confirm this action?');
+            if (confirm == true) {
+                decommissioned_robot++;
+            }
+            else {
+                
+            }
+        }
     }
+    alert(`Состояние роботов: готовы к работе: ${ready_robot}, требуется ремонт: ${needsRepair_robot}, списаны с работы: ${decommissioned_robot}`);
 }
+
+inventoryRobots();
