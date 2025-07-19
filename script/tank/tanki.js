486cf078ba8d46f8fcf32babeea4b82ebc5538a8
r1lUnreal <150723650+r1lUnreal@users.noreply.github.com>
2025-07-19 19:23:40 +0300

    update 19.07.2025 19:23

--- script/tank/tanki.js
+++ script/tank/tanki.js
@@ -3,19 +3,20 @@ const ctx = canvas.getContext('2d');
 
 let x = 200, dx = 2;
 
-// картинка игрока
+//? картинки
+//* картинка игрока
 player_image = new Image()
 player_image.src = 'image/unnamed.png'
 
-// картинка врага
+//* картинка врага
 vrag_image = new Image()
 vrag_image.src = 'image/4bf82c3946e82c7.png'
 
-// картинка пули
+//* картинка пули
 bullet_image = new Image()
 bullet_image.src = 'image/New Piskel.png'
 
-// вражеский танк 1
+//? вражеский танк 1
 function moveRect() {
     ctx.clearRect(0, 0, 1000, 700)
     ctx.drawImage(vrag_image, x, 200, 50, 50)
@@ -26,7 +27,7 @@ function moveRect() {
     }
 }
 
-// вражеский танк 2
+//? вражеский танк 2
 function moveRect1() {
     ctx.drawImage(vrag_image, 200, x, 50, 50)
     x += dx;
@@ -35,8 +36,7 @@ function moveRect1() {
     }
 }
 
-// moveRect()
-// словарь Игрок
+//? словарь Игрок
 player = {
     x: 400,
     y: 330,
@@ -48,13 +48,15 @@ player = {
     texture: player_image
 }
 
-// restart
+//? restart
+//* restart на кнопку
 const button = document.getElementById('Button');
-document.addEventListener('click', function() {
+button.addEventListener('click', function () {
     window.location.reload();
     console.log('Game restart');
 });
 
+//* restart на R
 document.addEventListener('keypress', function (event) {
     if (event.code == 'KeyR') {
         window.location.reload();
@@ -62,7 +64,8 @@ document.addEventListener('keypress', function (event) {
     }
 });
 
-// управление стрелками
+//? Управление персонажем
+//* управление стрелками
 document.addEventListener('keydown', function (event) {
     if (event.key == 'ArrowUp') {
         player.dy = -player.speed
@@ -87,7 +90,7 @@ document.addEventListener('keyup', function (event) {
     }
 });
 
-// управление WASD
+//* управление WASD
 document.addEventListener('keydown', function (event) {
     if (event.code == 'KeyW') {
         player.dy = -player.speed
@@ -119,9 +122,10 @@ function drawPleyer() {
     player.y += player.dy
 }
 
+//? колизии
 let stop = false;
 function collision() {
-    // если игрок касается врага
+    //* если игрок касается врага
     if (player.x >= x && player.x <= x + 50 && player.y >= 200 && player.y <= 260) {
         document.body.style.backgroundColor = 'red';
         player.dx = 0;
@@ -133,7 +137,7 @@ function collision() {
         ctx.fillText('you dead', 400, 350)
     }
 
-    // если пуля касается врага
+    //* если пуля касается врага
     if (bullet.x >= x && bullet.x <= x + 50 && bullet.y >= 200 && bullet.y <= 260) {
         document.body.style.backgroundColor = 'green';
         player.dx = 0;
@@ -146,7 +150,7 @@ function collision() {
     }
 }
 
-// параметры пули
+//? параметры пули
 bullet = {
     x: -10,
     y: -10,
@@ -159,6 +163,17 @@ function move_bullet() {
     bullet.y -= bullet.speed
 }
 
+//? стрельба
+//* выстрел на пробел
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Space') {
+        bullet.x = player.x;
+        bullet.y = player.y;
+        ctx.drawImage(bullet_image, player.x, player.y, 15, 15);
+    }
+})
+
+//* выстрел на ЛКМ
 canvas.addEventListener('click', function () {
     // document.body.style.backgroundColor = 'green'
     bullet.x = player.x;
@@ -166,6 +181,7 @@ canvas.addEventListener('click', function () {
     ctx.drawImage(bullet_image, player.x, player.y, 15, 15);
 });
 
+//? запуск игры на сайте
 function game() {
     if (stop == false) {
         moveRect();
